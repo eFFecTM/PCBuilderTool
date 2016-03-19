42ffcccd7772a6379d79824f9cccbe47b3a4d4de
killerboye <jan.delaet@hotmail.com>
2016-03-19 17:37:29 +0100

    Basic GUI almost completed.
     Still needs refinement of the details.
    
     ^Emre for you ;)

--- src/GUI.java
+++ src/GUI.java
@@ -1,14 +1,68 @@
 import javax.swing.*;
 
 /**
- * Created by Jan De Laet on 17/03/2016.
+ * Created by students UA:FTI-EI De Laet Jan & Yigit Yunus Emre on 17/03/2016.
  */
 public class GUI {
 
-    private JPanel panel1;
-    private JTabbedPane tabbedPane1;
-    private JTextArea welcomeToThePCBuilderTextArea;
-    private JTextField textField1;
-    private JTextArea textArea1;
+    //Main Pane
+    public JPanel mainPanel;
+    private JTabbedPane mainTabbedPanel;
+    private JTextArea welcomeTitle;
+    private JTextField loginText;
+    private JTextArea userCfgTitle;
+    private JList userCfgList;
+    private JLabel PCBuilderIcon;
+    private JProgressBar progressBar;
+
+    //Main Tabbed Panes
+    private JPanel catalogueTab;
+    private JPanel compareTab;
+    private JPanel wattTab;
+    private JPanel compatibilityTab;
+    private JPanel exportTab;
+
+    //catalogue Tab
+    private JTextArea detailsTextArea;
+    private JTextArea detailsTitleTextArea;
+    private JTextArea componentGroupTitle;
+    private JTextArea specificComponentTitle;
+    private JButton motherboardButton;
+    private JButton CPUButton;
+    private JButton RAMButton;
+    private JButton GPUButton;
+    private JButton PSUButton;
+    private JButton drivesButton;
+    private JButton addComponent;
+    private JButton addCompare;
+    private JLabel selectedComponentPicture;
+
+    //Compare Tab
+    private JTextArea component1;
+    private JTextArea component2;
     private JList list1;
+    private JList list2;
+    private JButton compareButton;
+    private JLabel picture1;
+    private JLabel picture2;
+
+
+    //Watt Usage Tab
+    private JTextArea wattInfo;
+    private JButton calculateButton;
+    private JTextArea wattResults;
+
+    //Compatibility Check Tab
+    private JTextArea compatibilityInfo;
+    private JButton compatibilityButton;
+    private JTextArea compatibilityResults;
+
+    //Export (offer file) Tab
+    private JTextArea exportInfo;
+    private JButton exportButton;
+    private JTextArea exportResults;
+
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import javax.swing.*;
+
+/**
+ * Created by students UA:FTI-EI De Laet Jan & Yigit Yunus Emre on 17/03/2016.
+ */
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        JFrame frame = new JFrame("PCBuilder Tool");
+        frame.setContentPane(new GUI().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
