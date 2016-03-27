c0d5d49beffe81c49e1132abfa34aeaabcc028c8
Jan De Laet <jan.delaet@hotmail.com>
2016-03-27 15:59:50 +0200

    Excel read basics
     ^Emre for you ;)

--- src/Component.java
+++ src/Component.java
@@ -2,7 +2,17 @@
  * Created by IMac-Windows on 27/03/2016.
  */
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Component
 {
@@ -12,6 +22,9 @@ public class Component
     private String nameComponent;
     private String otherDetails;
 
+    //read from Excel file
+    private int rowIndex;
+
     public Component()
     {
         ArrayList<String> details = new ArrayList<String>();
@@ -19,6 +32,7 @@ public class Component
         this.brandComponent = brandComponent;
         this.nameComponent = nameComponent;
         this.otherDetails = otherDetails;
+        rowIndex = 0;
     }
 
 
@@ -27,8 +41,57 @@ public class Component
 
     }
 
-    public void getDetails()
+    public int getDetails(int rowIndex)
     {
+        try
+        {
+            FileInputStream file = new FileInputStream(new File("Catalogue.xlsx"));
 
+            //Create Workbook instance holding reference to .xlsx file
+            XSSFWorkbook workbook = new XSSFWorkbook(file);
+
+            //Get first/desired sheet from the workbook
+            XSSFSheet sheetMotherboard = workbook.getSheetAt(0);
+            XSSFSheet sheetCPU = workbook.getSheetAt(1);
+            XSSFSheet sheetRAM = workbook.getSheetAt(2);
+
+            //Iterate through each rows one by one
+            Iterator<Row> rowIterator = sheetMotherboard.iterator();
+
+            while (rowIterator.hasNext())
+            {
+                Row row = rowIterator.next();
+                //For each row, iterate through all the columns
+                Iterator<Cell> cellIterator = row.cellIterator();
+
+                while (cellIterator.hasNext())
+                {
+                    Cell cell = cellIterator.next();
+                    //Check the cell type and format accordingly
+                    switch (cell.getCellType())
+                    {
+                        case Cell.CELL_TYPE_NUMERIC:
+                            System.out.print(cell.getNumericCellValue() + " ");
+                            break;
+                        case Cell.CELL_TYPE_STRING:
+                            System.out.print(cell.getStringCellValue() + " ");
+                            break;
+                    }
+                }
+                System.out.println("\n");
+            }
+            file.close();
+
+
+        } catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        return rowIndex++;
     }
 }
--- src/PCBuilder.java
+++ src/PCBuilder.java
@@ -1,4 +1,4 @@
-import javax.swing.*;
+import java.io.IOException;
 
 /**
  * Created by students UA:FTI-EI De Laet Jan & Yigit Yunus Emre on 17/03/2016.
@@ -7,14 +7,19 @@ import javax.swing.*;
 public class PCBuilder
 {
     private String loginName;
+    private Component component;
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
+        Component component = new Component();
+        int x = component.getDetails(0);
+/*
         JFrame frame = new JFrame("PCBuilder Tool");
         frame.setContentPane(new GUI().mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+ */
     }
 
 }
